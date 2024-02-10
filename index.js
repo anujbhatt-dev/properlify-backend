85cb3f5413051f222e5011a9e0a7ca4a4d3cb12d
anujbhatt-dev <anujbhatt023@gmail.com>
2024-02-10 16:52:51 +0530

    first commit

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use('/property',propertyRoute);
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT||5000;
 server.listen(PORT,()=>{
     console.log(`Server listening on port ${PORT}`);
 })
